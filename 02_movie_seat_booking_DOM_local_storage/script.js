e0c3ed4d4485c1a42defb38b92a0243400a0c5c0
agungTuanany <agung.tuanany@gmail.com>
2020-07-04 13:57:54 +0700

    Save Data To LocalStorage

--- 02_movie_seat_booking_DOM_local_storage/script.js
+++ 02_movie_seat_booking_DOM_local_storage/script.js
@@ -10,17 +10,31 @@ let ticketPrice = +movieSelect.value
 
 
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll(".row .seat.selected")
+    const selectedSeats      = document.querySelectorAll(".row .seat.selected")
     const selectedSeatsCount = selectedSeats.length
+
+    // Copy selected seats into array
+    // Map through array | map() is different with forEach(), forEach not return a result just loop through, map() return an array after loop through
+    // Return a new array indexes
+    const seatsIndex = [...selectedSeats].map((seat) => {
+        return [...seats].indexOf(seat)
+    })
+    console.log (seatsIndex)
+
+    localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex))
 
     count.innerText = selectedSeatsCount
     total.innerText = selectedSeatsCount * ticketPrice
-    console.log (selectedSeats)
+    // console.log (selectedSeats)
 }
+
+
+
 
 // Movie select event
 movieSelect.addEventListener("change", (event) => {
     ticketPrice = parseInt(event.target.value)
+    setMovieData(event.target.selectedIndex, event.target.value)
     updateSelectedCount()
 })
 
