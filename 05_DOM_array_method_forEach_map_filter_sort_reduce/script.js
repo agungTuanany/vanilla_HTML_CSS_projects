296090794c1246a490cb2cb3462f9dc6a2b9d6a3
agungTuanany <agung.tuanany@gmail.com>
2020-07-05 22:10:16 +0700

    Project-5 Sort by richest - sort()

--- 05_DOM_array_method_forEach_map_filter_sort_reduce/script.js
+++ 05_DOM_array_method_forEach_map_filter_sort_reduce/script.js
@@ -33,7 +33,12 @@ function doubleMoney() {
     updateDOM()
 }
 
+// Sort users by richest
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money)
 
+    updateDOM()
+}
 
 // Add new object to data array
 function addData(obj) {
@@ -64,3 +69,4 @@ function formatMoney(number) {
 // Event listeners
 addUserBtn.addEventListener("click", getRandomUser)
 doubleBtn.addEventListener("click", doubleMoney)
+sortBtn.addEventListener("click", sortByRichest)
