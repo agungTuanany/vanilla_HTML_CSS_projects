649b07da9520b2ce28de0bf6255a009c32ab08b3
agungTuanany <agung.tuanany@gmail.com>
2020-07-11 18:09:40 +0700

    project-9 Show transaction items

---
+++ 09_expense_tracker_array_method_local_storage/script.js
@@ -0,0 +1,45 @@
+const balance = document.getElementById("balance")
+const money_plus = document.getElementById("money-plus")
+const money_minus = document.getElementById("money-minus")
+const list = document.getElementById("list")
+const form = document.getElementById("form")
+const text = document.getElementById("text")
+const amount = document.getElementById("amount")
+
+
+const dummyTransactions = [
+    {id: 1, text: "Flower", amount: -20},
+    {id: 2, text: "Salary", amount: 300},
+    {id: 3, text: "Book", amount: -10},
+    {id: 4, text: "Hair cut", amount: -15},
+]
+
+let transaction = dummyTransactions
+
+
+// Add transaction to DOM list
+function addTransactionDOM(transaction) {
+     // Get sign
+    const sign = transaction.amount < 0 ? "-" : "+"
+    const item = document.createElement("li")
+
+    // Add class based on value
+    item.classList.add(transaction.amount < 0 ? "minus" : "plus")
+
+    item.innerHTML = `
+        ${transaction.text}<span>${sign}${Math.abs(transaction.amount)}</span><button class="delete-btn">x</button>
+    `;
+
+    list.appendChild(item)
+}
+
+// Init asap
+function init() {
+    list.innerHTML = ""
+
+    transaction.forEach(addTransactionDOM)
+}
+
+
+
+init()
