2a51554af5471101b494494215cd4c08fb7f6119
agungTuanany <agung.tuanany@gmail.com>
2020-07-12 12:09:06 +0700

    Project-10 index.html

--- 09_expense_tracker_array_method_local_storage/script.js
+++ 09_expense_tracker_array_method_local_storage/script.js
@@ -1,10 +1,10 @@
-const balance = document.getElementById("balance")
-const money_plus = document.getElementById("money-plus")
+const balance     = document.getElementById("balance")
+const money_plus  = document.getElementById("money-plus")
 const money_minus = document.getElementById("money-minus")
-const list = document.getElementById("list")
-const form = document.getElementById("form")
-const text = document.getElementById("text")
-const amount = document.getElementById("amount")
+const list        = document.getElementById("list")
+const form        = document.getElementById("form")
+const text        = document.getElementById("text")
+const amount      = document.getElementById("amount")
 
 // const dummyTransactions = [
 //     {id: 1, text: "Flower", amount: -20},
