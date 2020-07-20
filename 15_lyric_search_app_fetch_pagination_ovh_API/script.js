4ace5cbe131315a4db4818598e449814fbe2701e
agungTuanany <agung.tuanany@gmail.com>
2020-07-20 21:37:54 +0700

    Project-15 Update DOM Add Pagination | Prevent CORS error

--- 15_lyric_search_app_fetch_pagination_ovh_API/script.js
+++ 15_lyric_search_app_fetch_pagination_ovh_API/script.js
@@ -38,13 +38,65 @@ const searchSong = async term => {
     showData(data)
 }
 
+// XXX TODO: Use master-cards to fetch next 15 result XXX
 // Show song and artist in DOM
-const showData = data => {}
+const showData = data => {
+    // let output = ""
+
+    // data.data.forEach(song => {
+    //     output += `
+    //         <li>
+    //             <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+    //             <button class="btn" data-artist="${song.artist.name}"
+    //             data-songtitle="${song.title}">Get Lyrics</button>
+    //         </li>
+    //     `;
+    // })
+
+    // result.innerHTML = `
+    //     <ul class="songs">
+    //         ${output}
+    //     </ul>
+
+    // `;
+
+    result.innerHTML = `
+        <ul class="songs">
+            ${data.data.map(song => `
+                <li>
+                    <span><strong>${song.artist.name}</strong> - ${song.title}</span>
+                    <button class="btn" data-artist="${song.artist.name}"
+                    data-songtitle="${song.title}">Get Lyrics</button>
+                </li>
+            `)
+                    .join("")
+            }
+        </ul>
+    `;
+    if (data.prev || data.next) {
+        more.innerHTML = `
+            ${data.prev ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>` : ""}
+            ${data.next ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>` : ""}
+        `;
+    }
+    else {
+        more.innerHTML = ""
+    }
+}
+
+// Get prev and next songs
+async function getMoreSongs(url) {
+    /// Prevent to CORS error
+    const res =await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+    const data = await res.json()
+
+    showData(data)
+}
 
 
 // Ger random image for header
 async function renderItem() {
-    const res = await fetch("https://source.unsplash.com/1350x600?music")
+    const res  = await fetch("https://source.unsplash.com/1350x600?music")
     const data = await res.url
 
     header.style.backgroundImage = `url(${data})`
