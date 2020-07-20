aad564bdb9cfe73380ca927b0826393c8a333acf
agungTuanany <agung.tuanany@gmail.com>
2020-07-20 21:07:59 +0700

    Project-15 Fetching Song Artis

--- 15_lyric_search_app_fetch_pagination_ovh_API/script.js
+++ 15_lyric_search_app_fetch_pagination_ovh_API/script.js
@@ -1,9 +1,45 @@
-const header = document.querySelector("header")
+const header = document.getElementById("header")
+const form   = document.getElementById("form")
+const search = document.getElementById("search")
+const result = document.getElementById("result")
+const more   = document.getElementById("more")
+
+// Fetch resource
+const apiURL = "https://api.lyrics.ovh"
 
 
+// form event
+const formOnSubmit = event => {
+    event.preventDefault()
+
+    const searchTerm = search.value.trim()
 
 
+    if (!searchTerm) {
+        // XXX TODO: create your own modal XXX
+        alert("Please type in a seach term")
+    }
+    else {
+        searchSong(searchTerm)
+    }
+}
 
+// Seach by song or artist
+const searchSong = async term => {
+
+    // /// Using Promise
+    // fetch(`${apiURL}/suggest/${term}`)
+    //     .then(res => res.json())
+    //     .then(data => console.log(data) )
+
+    const res = await fetch(`${apiURL}/suggest/${term}`)
+    const data = await res.json()
+
+    showData(data)
+}
+
+// Show song and artist in DOM
+const showData = data => {}
 
 
 // Ger random image for header
@@ -16,4 +52,6 @@ async function renderItem() {
 
 renderItem()
 
+// event listener
+form.addEventListener("submit", formOnSubmit)
 
