63d4c7f9c848eccaeb3804f82748b177d894b46a
agungTuanany <agung.tuanany@gmail.com>
2020-07-21 17:24:42 +0700

    Project-16 Creating The large cirlce | conic-gradient

---
+++ 16_relaxer_app_css_animation_setTimeout/script.js
@@ -0,0 +1,11 @@
+const body = document.getElementById("body")
+
+// Ger random image for body
+async function renderItem() {
+    const res  = await fetch("https://source.unsplash.com/1350x600?relaxation")
+    const data = await res.url
+
+    const bg = body.style.background = `url(${data}) no-repeat center center/cover`
+}
+
+renderItem()
