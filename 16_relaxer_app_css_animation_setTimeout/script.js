4c3be7c295f89748525b5676bebed36045adcc81
agungTuanany <agung.tuanany@gmail.com>
2020-07-21 20:03:46 +0700

    Project-16 Breath animation trigger with JavaScript

--- 16_relaxer_app_css_animation_setTimeout/script.js
+++ 16_relaxer_app_css_animation_setTimeout/script.js
@@ -1,11 +1,38 @@
-const body = document.getElementById("body")
+const body      = document.getElementById("body")
+const container = document.getElementById("container")
+const text      = document.getElementById("text")
+
+const totalTime = 7500
+const breatheTime = (totalTime / 5 ) * 2
+const holdTime = totalTime / 5
+const totalTimeImage = 7200
+
+// Breathe animation
+function breatheAnimation() {
+    text.innerText = "breathe In!"
+    container.className = "container grow"
+
+    setTimeout(() => {
+        text.innerText = "Hold"
+
+        setTimeout(() => {
+            text.innerText = "Breathe Out!"
+            container.className = "container shrink"
+        }, holdTime)
+    }, breatheTime)
+}
+
 
 // Ger random image for body
 async function renderItem() {
-    const res  = await fetch("https://source.unsplash.com/1350x600?relaxation")
+    const res  = await fetch("https://source.unsplash.com/1350x600?hemp")
     const data = await res.url
 
     const bg = body.style.background = `url(${data}) no-repeat center center/cover`
 }
 
 renderItem()
+breatheAnimation()
+
+setInterval(renderItem, totalTimeImage)
+setInterval(breatheAnimation, totalTime)
