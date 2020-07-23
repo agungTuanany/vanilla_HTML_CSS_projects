b937e1813c2ad2c024ac2e5db65422b39db7c7c9
agungTuanany <agung.tuanany@gmail.com>
2020-07-23 18:41:38 +0700

    Project-20 Check Guessed Number

--- 20_number_guessing_game_speech_recognition/script.js
+++ 20_number_guessing_game_speech_recognition/script.js
@@ -18,8 +18,63 @@ recognition.start()
 function onSpeak(event) {
     const msg = event.results[0][0].transcript
 
-    // writeMessage(msg)
-    // checkNumber(msg)
+    writeMessage(msg)
+    checkNumber(msg)
+}
+
+// Write what user speak
+function writeMessage(msg) {
+    messageEl.innerHTML = `
+        <h3>You said:</h3>
+        <span class="box">${msg}</span>
+    `;
+}
+
+// Check messageEl againts element
+function checkNumber(msg) {
+    const num = +msg
+
+    // Check if valid number
+    if (Number.isNaN(num)) {
+        messageEl.innerHTML += `<div>${num} is not a valid number</div>`
+        return
+    }
+
+    // Check in range
+    if (num > 100 || num < 1) {
+        messageEl.innerHTML += '<div>Number must be between 1 and 100</div>'
+        return
+    }
+
+    // Check number
+    if (num === randomNum) {
+        document.body.innerHTML = `
+        <h2>Congrats! You have guessed the number!
+            <br>
+            <br>
+            it was ${num}
+        </h2>
+        <button id="play-again" class="play-again">Play Again</button>
+        `;
+    }
+    else if (num > randomNum) {
+        messageEl.innerHTML += '<div>GO LOWER</div>'
+    }
+    else {
+        messageEl.innerHTML += '<div>Go HIGHER</div>'
+    }
+}
+
+// End Speak Recognition service
+function onResume() {
+    recognition.start()
+}
+
+// Play again event
+function playAgainOnClick(event) {
+    if (event.target.id === 'play-again') {
+        window.location.reload()
+    }
 }
 
 function getRandomNumber() {
@@ -27,9 +82,9 @@ function getRandomNumber() {
 }
 
 
-// Speack result
+// Speak result event listener
 recognition.addEventListener("result", onSpeak)
-
-
+recognition.addEventListener("end", onResume)
+document.body,addEventListener("click", playAgainOnClick)
 
 
